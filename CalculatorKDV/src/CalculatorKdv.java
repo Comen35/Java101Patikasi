39284793852970fed8a967cf1fb74da5c7d65ab7
Comen35 <bahadir33@msn.com>
2022-05-30 21:42:47 +0300

    Calculating amount of KDV programme added.

--- CalculatorKDV/src/CalculatorKdv.java
+++ CalculatorKDV/src/CalculatorKdv.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 class CalculatorKdv{
+    
     static final int KDV_RATIO = 18;
+    static final int KDV_RATIO2 = 8;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         float price, priceWithKdv = 0, amountOfKdv;
@@ -9,11 +11,14 @@ class CalculatorKdv{
         System.out.print("Enter price please : ");
         price = input.nextFloat();
 
-        amountOfKdv = ( price * KDV_RATIO ) / 100;
+
+
+        amountOfKdv = ( price * (price > 1000 ? KDV_RATIO2 : KDV_RATIO)) / 100;
         priceWithKdv = price + amountOfKdv;
 
-        System.out.println("Price with KDV : " + priceWithKdv);
         System.out.println("Amount of KDV : " + amountOfKdv);
+        System.out.println("Price with KDV : " + priceWithKdv);
+        
 
         input.close();
         
