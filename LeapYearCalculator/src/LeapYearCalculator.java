1188062efaed4a3ac35a5a554f610267cc8a5c8e
Comen35 <bahadir33@msn.com>
2022-06-04 15:48:57 +0300

    isLeapYear name changed.

--- LeapYearCalculator/src/LeapYearCalculator.java
+++ LeapYearCalculator/src/LeapYearCalculator.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class LeapYearCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean isLeapYaer = true;
+        boolean isLeapYear = true;
 
         System.out.print("Enter year please : ");
         int year = input.nextInt();
 
         if( year % 4 != 0){
-            isLeapYaer = false;
+            isLeapYear = false;
         }
         else{
             if( year % 100 == 0 && year % 400 != 0){
-                isLeapYaer = false;
+                isLeapYear = false;
             }
         }
-        if(isLeapYaer){
+        if(isLeapYear){
             System.out.println(year + " is a leap year.");
         }
         else {
